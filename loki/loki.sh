659120b01ce644a68593b8961bca451a13c1594b
Katsuki Ryosuke <38076269+Honekatsu@users.noreply.github.com>
2025-03-06 21:03:42 +0900

    Delete loki directory

--- loki/loki.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-source ${PWD}/K-Loki/minio_loki/.minio_loki.env
-docker run --name loki -p 3100:3100 -v ${PWD}/K-Loki/loki/loki-config.yaml:/etc/loki/local-config.yaml grafana/loki
